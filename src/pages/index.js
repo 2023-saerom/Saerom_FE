1cf879208fe7317f91e11af43566de55948d5057
김상구 <spair0039@gmail.com>
2023-07-04 20:07:49 +0900

    맵 구현+현재위치 보이도록

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,21 @@
 import { Inter } from 'next/font/google'
+import { Map } from 'react-kakao-maps-sdk'
+import useCurrentLocation from '@/hooks/useLatLng';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const curLoc = useCurrentLocation()
+
+  console.log(curLoc)
+
+
   return (
     <>
-      예아
+      <Map
+        style={{width:"100%", height:"100vh"}}
+        center={{ lat: curLoc.latitude, lng: curLoc.longitude }}
+      />
     </>
   )
 }

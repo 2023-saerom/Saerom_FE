9f872f3793013bcef766335cf537219bea027160
김상구 <spair0039@gmail.com>
2023-07-04 15:15:53 +0900

    카카오 스크립트 위로 올림

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,12 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script type="text/javascript" strategy="beforeInteractive" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=da237d77970aaa05abc10bd97ad4702d?autoload=false" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
-      <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=da237d77970aaa05abc10bd97ad4702d" />
     </Html>
   )
 }

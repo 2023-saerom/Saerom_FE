9ea4b96b78f938c644557f31521354025fb14b04
김상구 <spair0039@gmail.com>
2023-07-05 16:50:50 +0900

    사이드네비 추가

---
+++ src/components/NavSidebar.jsx
@@ -0,0 +1,69 @@
+import { useRouter } from "next/router"
+import { styled } from "styled-components"
+
+export default function NavSidebar(){
+    const r = useRouter();
+
+    return(
+        <NavMainC>
+            <Logoimg src="/logo250.png"/>
+            <TabWrapper>
+                <Tabber className={r.pathname == '/'}>
+                    <img src="/sidenav/findPath.png"/>
+                </Tabber>
+                <Tabber className={r.pathname == '/search'}>
+                    <img src="/sidenav/search.png"/>
+                </Tabber>
+                <Tabber className={r.pathname == '/child/Loc'}>
+                    <img src="/sidenav/childLoc.png"/>
+                </Tabber>
+                <Tabber className={r.pathname == '/child/Add'}>
+                    <img src="/sidenav/childAdd.png"/>
+                </Tabber>
+                <Tabber className={r.pathname == '/addPath'}>
+                    <img src="/sidenav/addPath.png"/>
+                </Tabber>
+            </TabWrapper>
+        </NavMainC>
+    )
+}
+
+const NavMainC = styled.div`
+    z-index: 88;
+    display: flex;
+    flex-direction: column;
+    min-width: 100px;
+    width: 100px;
+    background: white;
+    border-right: 1px solid #F8D648;
+    height: 100vh;
+    padding: 30px 15px;
+    box-sizing: border-box;
+    gap: 60px;
+`
+
+const Logoimg = styled.img`
+    width: -webkit-fill-available;
+`
+
+const TabWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+const Tabber = styled.button`
+    height: 75px;
+    width: 75px;
+    background: white;
+    border: none;
+    border-radius: 99px;
+    transition: 250ms ease-in-out;
+
+    &:hover{
+        filter: brightness(95%);
+        cursor: pointer;
+        transition: 250ms ease-in-out;
+    }
+    &.true{
+        background: #FEFBED;
+    }
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,18 @@ import { styled } from 'styled-components';
 import { useState } from 'react';
 import { createPortal } from 'react-dom';
 import dynamic from 'next/dynamic';
+import NavSidebar from '@/components/NavSidebar';
 const AuthModal = dynamic(() => import('@/components/AuthModal'), { ssr: false });
 const MainMap = dynamic(()=> import('@/components/MainMap'), { ssr: false });
 
 export default function Home() {
     const [AuthPopupShowing, setAuthPopupShowing] = useState(false);
+    const isLogined = true;
     
     return (
         <MainFlex>
             {AuthPopupShowing ? createPortal(<AuthModal setState={setAuthPopupShowing}/>,document.body) : null}
+            {isLogined ? <NavSidebar/> : null}
             <SearchSidebar/>
             <MainMap />
         </MainFlex>

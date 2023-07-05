f2a3345f4928d282412d0d7b007705b464172ee5
김상구 <spair0039@gmail.com>
2023-07-05 20:07:25 +0900

    키즈 더미데이터

--- src/dumb.js
+++ src/dumb.js
@@ -36,3 +36,12 @@ export const dumbData = [
         name: '느그집', phNum:'010 4563 9999', info: '저는 가정교사 에요 문을 열어 주세요 꽝꽝', type: '던전'
     },
 ];
+
+export const dumbChild = [
+    {xes: 'female', Ph: '209 1293 1398', name: '초 갈'},
+    {xes: 'male', Ph: '209 2345 1293', name: '이름김'},
+    {xes: 'female', Ph: '209 1293 1398', name: '조조'},
+    {xes: 'male', Ph: '209 2345 1293', name: '장비'},
+    {xes: 'female', Ph: '209 1293 1398', name: '여포'},
+    {xes: 'male', Ph: '209 2345 1293', name: '이름김'},
+]

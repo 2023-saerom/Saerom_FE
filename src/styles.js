a3633c563153c73abd52c3006e7a10236d79c748
김상구 <spair0039@gmail.com>
2023-07-05 10:18:13 +0900

    스타일 중앙화

---
+++ src/styles.js
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+export const FlexCenter = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    &.ly{
+        background: #FEF7DA;
+    }
+`

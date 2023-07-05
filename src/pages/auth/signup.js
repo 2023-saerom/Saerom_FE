3c323e73a83f9d85ef3381074c6f7e801692d920
김상구 <spair0039@gmail.com>
2023-07-05 09:41:21 +0900

    레이아웃

--- src/pages/auth/signup.js
+++ src/pages/auth/signup.js
@@ -1,3 +1,4 @@
+import { jua } from "@/geo-si-gie/fonts"
 import { styled } from "styled-components"
 
 export default function SignUp(){
@@ -5,6 +6,17 @@ export default function SignUp(){
         <FlexCenter>
             <LoginMainContainer>
                 <img src="/signUpSide.png" />
+                <RightWing>
+                    <Header className={jua.className}>Sign Up</Header>
+                    <TextboxContainer>
+                        <TextBox/>
+                        <TextBox/>
+                        <TextBox/>
+                        <TextBox/>
+                        <TextBox/>
+                    </TextboxContainer>
+                    <Btn>회원가입</Btn>
+                </RightWing>
             </LoginMainContainer>
         </FlexCenter>
     )
@@ -18,6 +30,7 @@ const FlexCenter = styled.div`
     background-color: #FEF7DA;
 `
 const LoginMainContainer = styled.div`
+    display: flex;
     width: 1000px;
     height: 600px;
     background-color: #F8D648;
@@ -25,5 +38,29 @@ const LoginMainContainer = styled.div`
     box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.15);
 `
 const Header = styled.p`
-    font-family: var(-jua);
+    font-size: 42px;
+    margin: 0;
+`
+
+const RightWing = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 30px 50px;
+    width: 100%;
+    color: white;
+    gap: 56px;
+`
+
+const TextboxContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+`
+
+const TextBox = styled.input`
+    background-color: red;
+`
+
+const Btn = styled.button`
+    width: 100%;
 `

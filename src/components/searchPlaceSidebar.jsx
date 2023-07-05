3d493500f2b6871366b5b0e7dc38f1973bb35e10
김상구 <spair0039@gmail.com>
2023-07-05 17:44:44 +0900

    검색 사이드바

---
+++ src/components/searchPlaceSidebar.jsx
@@ -0,0 +1,11 @@
+import { Toppeer, SidebarBox } from "@/styles";
+
+export default function SearchPlaceSidebar(){
+    return(
+        <SidebarBox>
+            <Toppeer>
+                대충 검색
+            </Toppeer>
+        </SidebarBox>
+    )
+}

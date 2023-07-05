a643d7e5909cd5aa22d60243aefec702266dfabd
김상구 <spair0039@gmail.com>
2023-07-05 13:34:20 +0900

    인덴팅 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,24 @@
-import { Jua } from 'next/font/google'
 import { Map } from 'react-kakao-maps-sdk'
 import useCurrentLocation from '@/hooks/useLatLng';
 import SearchSidebar from '@/components/SearchSidebar';
 import { styled } from 'styled-components';
 
 export default function Home() {
-  const curLoc = useCurrentLocation()
-
-  return (
-    <MainFlex>
-      <SearchSidebar/>
-      <Map
-        style={{width: "calc(100vw - 530px)", height:"100vh"}}
-        center={{ lat: curLoc.latitude, lng: curLoc.longitude }}
-      >
-      </Map>
-    </MainFlex>
-  )
+    const curLoc = useCurrentLocation();
+    
+    
+    return (
+        <MainFlex>
+        <SearchSidebar/>
+            <Map style={{width: "calc(100vw - 530px)", height:"100vh"}} center={{ lat: curLoc.latitude, lng: curLoc.longitude }}>
+            </Map>
+        </MainFlex>
+    )
 }
-
+    
 const MainFlex = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: row;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: row;
 `

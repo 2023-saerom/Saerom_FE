3c87d151f10c3d53a5378c3ef2cb5d9a594a2533
김상구 <spair0039@gmail.com>
2023-07-05 19:46:52 +0900

    더미파일 분리하고 커밋은 안햇구나

--- src/components/SearchSidebar.jsx
+++ src/components/SearchSidebar.jsx
@@ -1,46 +1,11 @@
-import { styled } from "styled-components"
-import { SidebarBox, Toppeer } from "@/styles"
-import { useState } from "react"
+import { styled } from "styled-components";
+import { SidebarBox, Toppeer } from "@/styles";
+import { useState } from "react";
+import { ListWarp, PlaceRenault } from "./placeRenault";
+import { dumbData } from "@/dumb";
 
 export default function SearchSidebar(){
-    const [Data, setData] = useState([
-        {
-            name: '우리집', phNum:'011 2352 9999', info: '우리 집입니다 니가 왜검색해요개시발새기야', type: '가정집'
-        },
-        {
-            name: '느그집', phNum:'010 4563 9999', info: '저는 가정교사 에요 문을 열어 주세요 꽝꽝', type: '던전'
-        },
-        {
-            name: '우리집', phNum:'011 2352 9999', info: '우리 집입니다 니가 왜검색해요개시발새기야', type: '가정집'
-        },
-        {
-            name: '느그집', phNum:'010 4563 9999', info: '저는 가정교사 에요 문을 열어 주세요 꽝꽝', type: '던전'
-        },
-        {
-            name: '우리집', phNum:'011 2352 9999', info: '우리 집입니다 니가 왜검색해요개시발새기야', type: '가정집'
-        },
-        {
-            name: '느그집', phNum:'010 4563 9999', info: '저는 가정교사 에요 문을 열어 주세요 꽝꽝', type: '던전'
-        },
-        {
-            name: '우리집', phNum:'011 2352 9999', info: '우리 집입니다 니가 왜검색해요개시발새기야', type: '가정집'
-        },
-        {
-            name: '느그집', phNum:'010 4563 9999', info: '저는 가정교사 에요 문을 열어 주세요 꽝꽝', type: '던전'
-        },
-        {
-            name: '우리집', phNum:'011 2352 9999', info: '우리 집입니다 니가 왜검색해요개시발새기야', type: '가정집'
-        },
-        {
-            name: '느그집', phNum:'010 4563 9999', info: '저는 가정교사 에요 문을 열어 주세요 꽝꽝', type: '던전'
-        },
-        {
-            name: '우리집', phNum:'011 2352 9999', info: '우리 집입니다 니가 왜검색해요개시발새기야', type: '가정집'
-        },
-        {
-            name: '느그집', phNum:'010 4563 9999', info: '저는 가정교사 에요 문을 열어 주세요 꽝꽝', type: '던전'
-        },
-    ]);
+    const [Data, setData] = useState(dumbData);
 
     return(
         <SidebarBox>
@@ -53,25 +18,11 @@ export default function SearchSidebar(){
                 
             </Toppeer>
             <ListWarp>
-                {Data.map((d)=>{
-                    return <ListInner name={d.name} phNum={d.phNum} info={d.info} type={d.type}/>
+                {Data.map((d, idx)=>{
+                    return <PlaceRenault name={d.name} phNum={d.phNum} info={d.info} type={d.type} key={idx}/>
                 })}
             </ListWarp>
         </SidebarBox>
-    )
-}
-
-function ListInner(props){
-    return(
-        <ListInnerS>
-            <InnerH>{props.name}</InnerH>
-            <ComboPhType>
-                <InnerPH>{props.phNum}</InnerPH>
-                <span/>
-                <InnerInfor>{props.type}</InnerInfor>
-            </ComboPhType>
-            <InnerInfor>{props.info}</InnerInfor>
-        </ListInnerS>
     )
 }
 
@@ -95,47 +46,4 @@ const InputContainer = styled.div`
     flex-direction: column;
     gap: 8px;
     width: -webkit-fill-available;
-`
-
-const ListWarp = styled.div`
-    padding: 30px 50px;
-    overflow-y: scroll;
-    height: calc( 100vh - 174px - 60px );
-`
-
-const ListInnerS = styled.div`
-    display: flex;
-    gap: 5px;
-    flex-direction: column;
-    padding: 30px 0;
-    border-bottom: 1px #818181 solid;
-`
-const InnerH = styled.p`
-    font-size: 22px;
-    font-family: 'jua';
-    margin: 0;
-`
-const InnerPH = styled.p`
-    color: #257DDD;
-    font-size: 20px;
-    font-family: omyu pretty;
-    margin: 0;
-`
-const InnerInfor = styled.p`
-    font-size: 20px;
-    font-family: 'om';
-    margin: 0;
-`
-
-const ComboPhType = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 7px;
-    width: 100%;
-
-    &>span{
-        height: 10px;
-        border-left: black solid 1px;
-    }
 `

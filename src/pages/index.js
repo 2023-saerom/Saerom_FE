a2ffe627b46d6647bf70ab1cb68758d6cee68462
김상구 <spair0039@gmail.com>
2023-07-05 14:23:37 +0900

    모달

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,23 @@ import { Map } from 'react-kakao-maps-sdk'
 import useCurrentLocation from '@/hooks/useLatLng';
 import SearchSidebar from '@/components/SearchSidebar';
 import { styled } from 'styled-components';
+import { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+import dynamic from 'next/dynamic';
+const AuthModal = dynamic(() => import('@/components/AuthModal'), { ssr: false });
 
 export default function Home() {
     const curLoc = useCurrentLocation();
-    
+    const [AuthPopupShowing, setAuthPopupShowing] = useState(false);
+
+    useEffect(()=>{
+        setAuthPopupShowing(true);
+    },[])
     
     return (
         <MainFlex>
-        <SearchSidebar/>
+            {AuthPopupShowing ? createPortal(<AuthModal setState={setAuthPopupShowing}/>,document.body) : null}
+            <SearchSidebar/>
             <Map style={{width: "calc(100vw - 530px)", height:"100vh"}} center={{ lat: curLoc.latitude, lng: curLoc.longitude }}>
             </Map>
         </MainFlex>
